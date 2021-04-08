34e8a5d51fb448291638fb4c4b54e8f11532dcec
Alejandro Arteaga <alejow18@gmail.com>
2021-04-07 23:40:13 -0500

    rutas establecidas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { PrimeNgModule } from './prime-ng/prime-ng.module';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
+import { AppRouterModule } from './app-router.module';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { VentasModule } from './ventas/ventas.module';
     BrowserModule,
     PrimeNgModule,
     SharedModule,
-    VentasModule
+    VentasModule,
+    AppRouterModule
     
   ],
   providers: [],
